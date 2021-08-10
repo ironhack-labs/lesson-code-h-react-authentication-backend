74d89c79d9acd499dadbbf6c0511f88360cea80a
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-10 18:47:02 +0200

    Example 6 - Update app.js and protect routes with JWT middleware

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ require("dotenv/config");
 require("./db");
 const express = require("express");
 
-const app = express();
+const { isAuthenticated } = require("./middleware/jwt.middleware"); // <== IMPORT
 
+
+const app = express();
 require("./config")(app);
+
 
 // 👇 Start handling routes here
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 const projectRouter = require("./routes/project.routes");
-app.use("/api", projectRouter);
+app.use("/api", isAuthenticated, projectRouter);            // <== UPDATE
 
 const taskRouter = require("./routes/task.routes");
-app.use("/api", taskRouter);
+app.use("/api", isAuthenticated, taskRouter);            // <== UPDATE
 
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
