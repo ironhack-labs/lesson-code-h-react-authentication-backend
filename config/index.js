37fc3765cbe94f9052561cb710a1de90da9e2b34
Devin Fiachra <devinduffymeade@gmail.com>
2023-07-19 14:56:50 +0200

    update fallbackroute for local frontend app

--- config/index.js
+++ config/index.js
@@ -11,6 +11,9 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
+
+const client = process.env.CLIENT_URL || "http://localhost:5005"
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -21,7 +24,7 @@ module.exports = (app) => {
   
   app.use(
     cors({
-      origin: ["https://bookwormio.netlify.app/"],
+      origin: [client],
     })
   );
 
